dbd428a88c363a5b111b8ef56a276726d71f5b59
asdf <asdf@asdf.ru>
2016-11-09 21:01:29 +0300

    First commit with gradle

---
+++ src/ru/innopolis/uni/course2/Main.java
@@ -0,0 +1,10 @@
+package ru.innopolis.uni.course2;
+
+/**
+ * Created by olymp on 09.11.2016.
+ */
+public class Main {
+    static public void main(String[] ar) {
+        System.out.println("Hello world");
+    }
+}
