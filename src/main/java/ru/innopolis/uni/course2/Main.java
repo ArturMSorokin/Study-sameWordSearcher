a9cb4eeb0493ff3ae1a5af7e833db1c428de5a64
asdf <asdf@asdf.ru>
2016-11-10 22:57:39 +0300

    logging added

--- src/main/java/ru/innopolis/uni/course2/Main.java
+++ src/main/java/ru/innopolis/uni/course2/Main.java
@@ -3,8 +3,13 @@ package ru.innopolis.uni.course2;
 /**
  * Created by olymp on 09.11.2016.
  */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
     static public void main(String[] ar) {
+
         System.out.println("Hello world");
+        logger.error("someError");
     }
 }
---
+++ src/main/java/ru/innopolis/uni/course2/TextParseThread.java
@@ -0,0 +1,8 @@
+package ru.innopolis.uni.course2;
+
+/**
+ * Created by olymp on 10.11.2016.
+ */
+public class TextParseThread {
+
+}
